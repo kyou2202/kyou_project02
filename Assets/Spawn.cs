e998bcd28c72b363b115e00e163fc19b5b34d00b
KaarHeredia <kaar2202@gmail.com>
2023-10-09 11:02:36 -0600

    CuboSpawnMovingShooting

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //clase
+    //tipo de dato, nombre <-- variable
+
+    //objeto a spawnear
+     public GameObject objectToSpawn;
+
+    //coordenadas, variable para guardar 3 numeros <-- vector
+     Vector3 spawnPoint = new Vector3(0, 0 , 0);
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(objectToSpawn, spawnPoint, Quaternion.Euler(5, 7, 8));
+        }
     }
 }
