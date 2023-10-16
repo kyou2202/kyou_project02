f6dde261e5834556631243ccb957f4c08cf3d498
kyou_2202 <kaar2202@gmail.com>
2023-10-16 10:38:08 -0600

    MovementKeys

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    //clase
-    //tipo de dato, nombre <-- variable
-
-    //objeto a spawnear
      public GameObject objectToSpawn;
-
-    //coordenadas, variable para guardar 3 numeros <-- vector
      Vector3 spawnPoint = new Vector3(0, 0 , 0);
+     public float speed;
 
     void Start()
     {
@@ -22,7 +17,17 @@ public class Spawn : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(objectToSpawn, spawnPoint, Quaternion.Euler(5, 7, 8));
+            Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down.normalized * speed * Time.deltaTime);
         }
     }
 }
